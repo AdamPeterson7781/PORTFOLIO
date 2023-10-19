98c72f6f918e4faa80cb5fb13055fa08c5109d22
Shomurotov Otamurod <134951919+AdamPeterson7781@users.noreply.github.com>
2023-10-19 19:22:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let navSvgIcon = document.querySelector('.nav-svg');
+let svg = document.querySelector('#svg');
+let secondSvg = document.querySelector('#second-svg');
+let navTxt1 = document.querySelector('#nav-txt-1');
+let navTxt2 = document.querySelector('#nav-txt-2');
+let navTxt3 = document.querySelector('#nav-txt-3');
+
+let isNavVisible = false;
+
+function navSvg() {
+  if (!isNavVisible) {
+    navTxt1.style.display = "block";
+    navTxt2.style.display = "block";
+    navTxt3.style.display = "block";
+
+    svg.style.display = "none";
+    secondSvg.style.display = "block"
+  } else {
+    navTxt1.style.display = "none";
+    navTxt2.style.display = "none";
+    navTxt3.style.display = "none";
+
+    svg.style.display = "block";
+    secondSvg.style.display = "none"
+  }
+  isNavVisible = !isNavVisible; // Toggle the flag
+}
+
+navSvgIcon.addEventListener("click", toggleNav);
+
+
+
